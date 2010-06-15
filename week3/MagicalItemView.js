958e571e072b1f32c5f8300579df4d19e02599e2
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2010-06-14 19:41:11 -0700

    modifications made in class

--- week3/MagicalItemView.js
+++ week3/MagicalItemView.js
@@ -6,10 +6,32 @@
      
      //the containing node (div) to which this magical item view should be added
      this.containerNode = containerNode;
-     this.magicalItem = itemModel;
+     this.item = itemModel;
      
      this.draw = function() {
          
          //create my DOM nodes and add them to the container
+                
+        var itemDiv = document.createElement('div');
+        itemDiv.className = 'catalogItem';
+        this.containerNode.appendChild(itemDiv);
+        
+        var nameDiv = document.createElement('div');
+        nameDiv.className = 'catalogItemName';
+        nameDiv.innerHTML = this.item.name;
+        itemDiv.appendChild(nameDiv);
+        
+        var descriptionDiv = document.createElement('div');
+        descriptionDiv.className = 'catalogItemDescription';
+        descriptionDiv.innerHTML = this.item.description;
+        itemDiv.appendChild(descriptionDiv);
+        
+        var priceDiv = document.createElement('div');
+        priceDiv.className = 'catalogItemPrice';
+        itemDiv.appendChild(priceDiv);
+        var priceLabel = document.createTextNode("Price: $");
+        priceDiv.appendChild(priceLabel);
+        var priceValue = document.createTextNode(this.item.price);
+        priceDiv.appendChild(priceValue);
      }
  }
