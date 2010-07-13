bb1865fb40fca41f7af65f774b7b16df9ff02de7
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2010-07-12 20:23:24 -0700

    modification to materials from class plus slides

--- week6/MagicalItem.js
+++ week6/MagicalItem.js
@@ -1,30 +1,38 @@
 //if the namespace hasn't been defined then define it now
 if (!magical_store) magical_store = {};
 
-magical_store.MagicalItem = function() {
-                
-    this.price = 0;
-    this.name = '';
-    this.description = '';
-    this.hitPoints = 0;
-    this.mana = 0;
-    this.kilos = 0;
+//for a scalable solution use prototype for you method defintiions
+magical_store.MagicalItem = function(dataHash) {
+    
+    this.initWithData(dataHash);
+    
     this.isPuffOfSmoke = false;
     
-    this.damage = function(numManaPoints) {
-        this.mana -= numManaPoints;
-                    
-        if (this.mana <= 0) this.isPuffOfSmoke = true; 
+}
+
+magical_store.MagicalItem.prototype.initWithData = function(dataHash) {
+    this.price = dataHash.price;
+    this.name = dataHash.name;
+    this.description = dataHash.description;
+    this.hitPoints = dataHash.hitPoints;
+    this.mana = dataHash.mana;
+    this.kilos = dataHash.kilos;
+}
+    
+    
+magical_store.MagicalItem.prototype.damage = function(numManaPoints) {
+    this.mana -= numManaPoints;
+                
+    if (this.mana <= 0) this.isPuffOfSmoke = true; 
+}
+
+magical_store.MagicalItem.prototype.regenerate = function(numManaPoints) {
+    
+    if (this.isPuffOfSmoke) {
+        this.mana = numManaPoints;
+        this.isPuffOfSmoke = false;
+    } else {
+        this.mana += numManaPoints;
     }
     
-    this.regenerate = function(numManaPoints) {
-        
-        if (this.isPuffOfSmoke) {
-            this.mana = numManaPoints;
-            this.isPuffOfSmoke = false;
-        } else {
-            this.mana += numManaPoints;
-        }
-        
-    }
-}
+}         
